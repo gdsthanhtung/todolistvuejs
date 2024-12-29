dacd5295d82c2140c8f5446b960f988af31a8eb0
“GdsThanhTung” <gds.thanhtung@gmail.com>
2024-12-29 11:13:05 +0700

    Tach bootstrap ra thanh folder plugin rieng

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,13 @@
             <div class="col-12">
               <div class="form-group">
                 <div class="dropdown">
-                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Sort by
-                  </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Name ASC</a>
-                    <a class="dropdown-item" href="#">Name DESC</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Level ASC</a>
-                    <a class="dropdown-item" href="#">Level DESC</a>
-                  </div>
+                  <b-dropdown id="dropdown-1" text="Sort by" class="">
+                    <b-dropdown-item>Name ASC</b-dropdown-item>
+                    <b-dropdown-item>Name DESC</b-dropdown-item>
+                    <b-dropdown-divider></b-dropdown-divider>
+                    <b-dropdown-item active>Level ASC</b-dropdown-item>
+                    <b-dropdown-item>Level DESC</b-dropdown-item>
+                  </b-dropdown>
                   <span class="badge badge-success badge-medium">NAME - DESC</span>
                 </div>
               </div>
